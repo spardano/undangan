797bca5c9f361133d85251c89a9dd08c01102625
spardano <sakti.pardano29@gmail.com>
2020-10-01 11:29:19 +0700

    ubah jam

--- index.php
+++ index.php
@@ -1,237 +1,179 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
 
-    <!-- Bootstrap CSS -->
-    <link
-      rel="stylesheet"
-      href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-      integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
-      crossorigin="anonymous"
-    />
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous" />
 
-    <link
-      rel="stylesheet"
-      href="assets/font/fontawesome-free-5.14.0-web/css/all.css"
-    />
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
 
-    <link rel="stylesheet" href="assets/css/style.css" />
+  <link rel="stylesheet" href="assets/font/fontawesome-free-5.14.0-web/css/all.css" />
 
-    <link
-      href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&family=Parisienne&display=swap"
-      rel="stylesheet"
-    />
+  <link rel="stylesheet" href="assets/css/style.css" />
 
-    <title>Wedding Anouncement Youlin & Wawan</title>
-  </head>
-  <body>
-    <iframe
-      src="assets/music/silence.mp3"
-      allow="autoplay"
-      id="audio"
-      style="display: none"
-    ></iframe>
-    <audio autoplay loop id="playaudio">
-      <source src="assets/music/mine.mp3" />
-    </audio>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&family=Parisienne&display=swap" rel="stylesheet" />
 
-    <div class="jumbotron jumbotron-fluid shadow-lg">
-      <div class="container text-center">
-        <img src="assets/img/bismillah2.png" class="img-fluid" alt="" />
+  <title>Wedding Anouncement Youlin & Wawan</title>
+</head>
 
-        <div
-          class="walimatul p-3 col-sm-4 offset-sm-4"
-          data-aos="fade-up"
-          data-aos-duration="1000"
-        >
-          <h1>Walimatul 'Urs</h1>
+<body>
+  <iframe src="assets/music/silence.mp3" allow="autoplay" id="audio" style="display: none"></iframe>
+  <audio autoplay loop id="playaudio">
+    <source src="assets/music/mine.mp3" />
+  </audio>
+
+  <div class="jumbotron jumbotron-fluid shadow-lg">
+    <div class="container text-center">
+      <img src="assets/img/bismillah2.png" class="img-fluid" alt="" />
+
+      <div class="walimatul p-3 col-sm-4 offset-sm-4" data-aos="fade-up" data-aos-duration="1000">
+        <h1>Walimatul 'Urs</h1>
+      </div>
+
+      <p class="lead" data-aos="fade-up" data-aos-easing="linear" data-aos-duration="1500">
+        Assalamu’alaikum Warahmatullahi Wabarakatuh <br />
+        Dengan memohon rahmat serta ridho Allah Subhanahu wa ta'ala,
+        perkenankanlah kami menyelenggarakan acara pernikahan putra-putri kami
+        <br />
+      </p>
+
+      <i class="fa fa-chevron-down fa-3x" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="2000"></i>
+    </div>
+  </div>
+
+  <div class="arrow rounded-circle mx-auto" data-aos="zoom-in">
+    <img src="assets/img/arrow.png" alt="" />
+  </div>
+
+  <section id="pengantin">
+    <div class="container">
+      <div class="nama col-sm-8 offset-sm-2" data-aos="fade-up" data-aos-duration="1800">
+        <div class="garis col-sm text-center">
+          <!-- Pengantin wanita -->
+          <h1>Yuolin Dita Humairah, S.T</h1>
+          <p>Putri Dari Bpk. Ir. Budi Azhari & Ibu Dra. Yulhemita</p>
+
+          <img src="assets/img/ringsilver.png" alt="" />
+
+          <!-- Penganti Pria -->
+          <h1>Wawan Aries Sandi, S.T</h1>
+          <p>Putra Dari Bpk. Wahidul Kahar & Ibu Zulhayati</p>
         </div>
-
-        <p
-          class="lead"
-          data-aos="fade-up"
-          data-aos-easing="linear"
-          data-aos-duration="1500"
-        >
-          Assalamu’alaikum Warahmatullahi Wabarakatuh <br />
-          Dengan memohon rahmat serta ridho Allah Subhanahu wa ta'ala,
-          perkenankanlah kami menyelenggarakan acara pernikahan putra-putri kami
-          <br />
-        </p>
-
-        <i
-          class="fa fa-chevron-down fa-3x"
-          data-aos="fade-down"
-          data-aos-easing="linear"
-          data-aos-duration="2000"
-        ></i>
       </div>
     </div>
+  </section>
 
-    <div class="arrow rounded-circle mx-auto" data-aos="zoom-in">
-      <img src="assets/img/arrow.png" alt="" />
+  <section id="timeandplace">
+    <div class="header text-center" data-aos="zoom-in">
+      <h1>Tempat dan Waktu</h1>
+      <hr />
     </div>
 
-    <section id="pengantin">
-      <div class="container">
-        <div
-          class="nama col-sm-8 offset-sm-2"
-          data-aos="fade-up"
-          data-aos-duration="1800"
-        >
-          <div class="garis col-sm text-center">
-            <!-- Pengantin wanita -->
-            <h1>Yuolin Dita Humairah, S.T</h1>
-            <p>Putri Dari Bpk. Ir. Budi Azhari & Ibu Dra. Yulhemita</p>
+    <div class="container">
+      <div class="row">
+        <div class="time col-sm-5 mx-auto shadow-lg" data-aos="fade-up" data-aos-duration="1800">
+          <div class="logo rounded-circle mx-auto text-center">
+            <i class="fas fa-calendar-alt fa-3x mt-3"></i>
+          </div>
+          <div class="content text-center mt-4">
+            <h2>Rabu, 28 Oktober 2020</h2>
+            <p>9:00 Pagi Sampai Selesai</p>
 
-            <img src="assets/img/ringsilver.png" alt="" />
+            <div class="countdown col-sm-5 mx-auto mt-4">
+              <p id="hitungmundur" class="mb-4"></p>
+            </div>
+          </div>
+        </div>
 
-            <!-- Penganti Pria -->
-            <h1>Wawan Aries Sandi, S.T</h1>
-            <p>Putra Dari Bpk. Wahidul Kahar & Ibu Zulhayati</p>
+        <div class="place col-sm-5 mx-auto shadow-lg" data-aos="fade-up" data-aos-duration="1800">
+          <div class="logo rounded-circle mx-auto text-center">
+            <i class="fas fa-map-marker-alt fa-3x mt-3"></i>
+          </div>
+
+          <div class="content text-center mt-4">
+            <p>
+              Jl. Angkatan Lrg. Jambu, Kec. Kemuning, Kota Palembang, Sumatera
+              Selatan
+            </p>
+            <img src="assets/img/mappesta.jpeg" class="img-thumbnail my-3" alt="" />
+
+            <a href="https://goo.gl/maps/NwbCW6UYyThpiTNQ8" class="btn btn-primary">Buka Google Map</a>
           </div>
         </div>
       </div>
-    </section>
+    </div>
+  </section>
 
-    <section id="timeandplace">
+  <section id="note">
+    <div class="container">
       <div class="header text-center" data-aos="zoom-in">
-        <h1>Tempat dan Waktu</h1>
+        <h1>Tutur Kata</h1>
         <hr />
       </div>
 
-      <div class="container">
-        <div class="row">
-          <div
-            class="time col-sm-5 mx-auto shadow-lg"
-            data-aos="fade-up"
-            data-aos-duration="1800"
-          >
-            <div class="logo rounded-circle mx-auto text-center">
-              <i class="fas fa-calendar-alt fa-3x mt-3"></i>
-            </div>
-            <div class="content text-center mt-4">
-              <h2>Rabu, 28 Oktober 2020</h2>
-              <p>10:00 Pagi Sampai Selesai</p>
+      <div class="content" data-aos="fade-up" data-aos-duration="1800">
+        <div class="col-sm-6 mx-auto mt-4">
+          <p>
+            Kami segenap keluarga besar mengucapkan terimakasih atas do'a dan
+            restu Bapak/Ibu/Saudara/i.
+          </p>
 
-              <div class="countdown col-sm-5 mx-auto mt-4">
-                <p id="hitungmundur" class="mb-4"></p>
-              </div>
-            </div>
-          </div>
-
-          <div
-            class="place col-sm-5 mx-auto shadow-lg"
-            data-aos="fade-up"
-            data-aos-duration="1800"
-          >
-            <div class="logo rounded-circle mx-auto text-center">
-              <i class="fas fa-map-marker-alt fa-3x mt-3"></i>
-            </div>
-
-            <div class="content text-center mt-4">
-              <p>
-                Jl. Angkatan Lrg. Jambu, Kec. Kemuning, Kota Palembang, Sumatera
-                Selatan
-              </p>
-              <img
-                src="assets/img/mappesta.jpeg"
-                class="img-thumbnail my-3"
-                alt=""
-              />
-
-              <a
-                href="https://goo.gl/maps/NwbCW6UYyThpiTNQ8"
-                class="btn btn-primary"
-                >Buka Google Map</a
-              >
-            </div>
-          </div>
+          <p>
+            Sesuai dengan imbauan pemerintah terkait penanganan pandemi COVID
+            19, acara akad akan dilaksanakan sesuai dengan protokol kesehatan
+            dan aturan dari pemerintah.
+          </p>
         </div>
       </div>
-    </section>
+    </div>
+  </section>
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="assets/js/jquery-3.5.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
-    <section id="note">
-      <div class="container">
-        <div class="header text-center" data-aos="zoom-in">
-          <h1>Tutur Kata</h1>
-          <hr />
-        </div>
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 
-        <div class="content" data-aos="fade-up" data-aos-duration="1800">
-          <div class="col-sm-6 mx-auto mt-4">
-            <p>
-              Kami segenap keluarga besar mengucapkan terimakasih atas do'a dan
-              restu Bapak/Ibu/Saudara/i.
-            </p>
+  <script>
+    // Set the date we're counting down to
+    var countDownDate = new Date("Oct 28, 2020 09:00:00").getTime();
 
-            <p>
-              Sesuai dengan imbauan pemerintah terkait penanganan pandemi COVID
-              19, acara akad akan dilaksanakan sesuai dengan protokol kesehatan
-              dan aturan dari pemerintah.
-            </p>
-          </div>
-        </div>
-      </div>
-    </section>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="assets/js/jquery-3.5.1.min.js"></script>
-    <script
-      src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-      integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
-      integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
-      crossorigin="anonymous"
-    ></script>
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+      // Get today's date and time
+      var now = new Date().getTime();
 
-    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+      // Find the distance between now and the count down date
+      var distance = countDownDate - now;
 
-    <script>
-      // Set the date we're counting down to
-      var countDownDate = new Date("Oct 28, 2020 08:00:00").getTime();
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-      // Update the count down every 1 second
-      var x = setInterval(function () {
-        // Get today's date and time
-        var now = new Date().getTime();
+      // Output the result in an element with id="demo"
+      document.getElementById("hitungmundur").innerHTML =
+        days + " : " + hours + " : " + minutes + " : " + seconds;
 
-        // Find the distance between now and the count down date
-        var distance = countDownDate - now;
+      // If the count down is over, write some text
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("hitungmundur").innerHTML = "EXPIRED";
+      }
+    }, 1000);
+  </script>
 
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor(
-          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-        );
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  <script>
+    AOS.init();
+  </script>
+</body>
 
-        // Output the result in an element with id="demo"
-        document.getElementById("hitungmundur").innerHTML =
-          days + " : " + hours + " : " + minutes + " : " + seconds;
-
-        // If the count down is over, write some text
-        if (distance < 0) {
-          clearInterval(x);
-          document.getElementById("hitungmundur").innerHTML = "EXPIRED";
-        }
-      }, 1000);
-    </script>
-
-    <script>
-      AOS.init();
-    </script>
-  </body>
 </html>
